771eadfc832744f0388d0435bbafebc2ac599556
idleberg <git@idleberg.com>
2020-03-19 01:04:37 +0100

    replace buildUrl with query-string module

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,5 @@
 import * as fetch from 'isomorphic-fetch';
-
-let checkStatus = (response: Response): Promise<Response> => {
-  if (response.status >= 200 && response.status < 300) {
-    return Promise.resolve(response);
-  } else {
-    return Promise.reject(new Error(response.statusText));
-  }
-};
-
-let parseJson = (response: Response): Promise<Response> => {
-  return response.json();
-};
-
-let buildUrl = (key: string, input: Object): string => {
-  if (Array.isArray(input)) {
-    let reqUrl = '';
-
-    input.forEach( r => {
-      r = (typeof r === 'string') ? r.toUpperCase() : r;
-      reqUrl += `&${key}=${r}`;
-    });
-
-    return reqUrl;
-  }
-
-  return `&${key}=${input}`;
-};
-
-let callAPI = (url: string) => {
-  return fetch(url)
-  .then(checkStatus)
-  .then(parseJson)
-  .then( response => {
-    return response.data;
-  }).catch( error => {
-    console.error(error);
-  });
-};
+import * as queryString from 'query-string';
 
 export default class WienerLinien {
   API_KEY: string;
@@ -54,15 +17,15 @@ export default class WienerLinien {
    * @param {Object} [options] - optional arguments
    * @returns {Object} - JSON
    */
-  monitor(rbl: number|string|number[]|string[], options: ActiveTrafficInfo = {}) {
+  public monitor(rbl: number|string|number[]|string[], options: ActiveTrafficInfo = {}) {
     this.reqUrl = `${this.baseUrl}/monitor?sender=${this.API_KEY}`;
-    this.reqUrl += buildUrl('rbl', rbl);
+    this.reqUrl += queryString.stringify({rbl: rbl});
 
     if (typeof options.activateTrafficInfo !== 'undefined' && options.activateTrafficInfo) {
-      this.reqUrl += buildUrl('activateTrafficInfo', options.activateTrafficInfo);
+      this.reqUrl += queryString.stringify({activateTrafficInfo: options.activateTrafficInfo});
     }
 
-    return callAPI(this.reqUrl);
+    return this.callAPI(this.reqUrl);
   }
 
   /**
@@ -70,22 +33,22 @@ export default class WienerLinien {
    * @param {Object} [options] - optional arguments
    * @returns {Object} - JSON
    */
-  newsList(options: RelatedInfo = {}) {
+  public newsList(options: RelatedInfo = {}) {
     this.reqUrl = `${this.baseUrl}/newsList?sender=${this.API_KEY}`;
 
     if (typeof options.relatedLine !== 'undefined' && options.relatedLine) {
-      this.reqUrl += buildUrl('relatedLine', options.relatedLine);
+      this.reqUrl += queryString.stringify({relatedLine: options.relatedLine});
     }
 
     if (typeof options.relatedStop !== 'undefined' && options.relatedStop) {
-      this.reqUrl += buildUrl('relatedStop', options.relatedStop);
+      this.reqUrl += queryString.stringify({relatedStop: options.relatedStop});
     }
 
     if (typeof options.name !== 'undefined' && options.name) {
-      this.reqUrl += buildUrl('name', options.name);
+      this.reqUrl += queryString.stringify({name: options.name});
     }
 
-    return callAPI(this.reqUrl);
+    return this.callAPI(this.reqUrl);
   }
 
   /**
@@ -93,21 +56,41 @@ export default class WienerLinien {
    * @param {Object} [options] - optional arguments
    * @returns {Object} - JSON
    */
-  trafficInfoList(options: RelatedInfo = {}) {
+  public trafficInfoList(options: RelatedInfo = {}) {
     this.reqUrl = `${this.baseUrl}/trafficInfoList?sender=${this.API_KEY}`;
 
     if (typeof options.relatedLine !== 'undefined' && options.relatedLine) {
-      this.reqUrl += buildUrl('relatedLine', options.relatedLine);
+      this.reqUrl += queryString.stringify({relatedLine: options.relatedLine});
     }
 
     if (typeof options.relatedStop !== 'undefined' && options.relatedStop) {
-      this.reqUrl += buildUrl('relatedStop', options.relatedStop);
+      this.reqUrl += queryString.stringify({relatedStop: options.relatedStop});
     }
 
     if (typeof options.name !== 'undefined' && options.name) {
-      this.reqUrl += buildUrl('name', options.name);
+      this.reqUrl += queryString.stringify({name: options.name});
     }
 
-    return callAPI(this.reqUrl);
+    return this.callAPI(this.reqUrl);
   }
+
+  private checkStatus(response: Response): Promise<Response> {
+    if (response.status >= 200 && response.status < 300) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+  };
+
+  private parseJson (response: Response): Promise<Response> {
+    return response.json();
+  };
+
+  private callAPI(url: string) {
+    return fetch(url)
+      .then(this.checkStatus)
+      .then(this.parseJson)
+      .then(response => response.data)
+      .catch(console.error);
+  };
 }
